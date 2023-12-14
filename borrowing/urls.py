04d1ee1589cd60863668a08d39f4115ec069c1d6
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-14 14:17:32 +0200

    check no return twise

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from .views import BorrowingReturnView
+
 urlpatterns = [
 
 ]
+
+
 
 app_name = "borrowing"
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
+from rest_framework import generics, status
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -12,3 +15,25 @@ from borrowing.serializers import (
 
 class BorrowingViewSet():
     pass
+
+
+class BorrowingReturnView(generics.UpdateAPIView):
+    queryset = Borrowing.objects.select_related('book', 'user')
+    serializer_class = BorrowingSerializer
+
+    def update(self, request, *args, **kwargs):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_date is not None:
+            return Response(
+                {'message': 'Book already returned.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        borrowing.actual_return_date = timezone.now()
+
+
+        return Response(
+            BorrowingSerializer(borrowing).data,
+            status=status.HTTP_200_OK
+        )
