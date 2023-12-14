2c25ada7dddc3acad287f40e9084afbae7f882c6
Roman Zhadan <21gehassig@gmail.com>
2023-12-14 15:02:26 +0200

    implement list & detail endpoints

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-urlpatterns = [
+from borrowing.views import (
+    BorrowingViewSet,
+)
 
-]
+router = routers.DefaultRouter()
+router.register("borrowing", BorrowingViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "borrowing"
--- borrowing/views.py
+++ borrowing/views.py
@@ -10,5 +10,23 @@ from borrowing.serializers import (
 )
 
 
-class BorrowingViewSet():
-    pass
+class BorrowingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        elif self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
