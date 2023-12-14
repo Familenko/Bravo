b8539f09c367152414719cefc09137f9f8749d50
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-12-14 16:55:06 +0200

    Create serializer for book model

--- book/serializers.py
+++ book/serializers.py
@@ -7,15 +7,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
-
-
-class BookListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        field = "__all__"
-
-
-class BookDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = "__all__"
