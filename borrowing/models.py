450ccf46d6d9c291032f20863b0d63658c76d34b
Roman Zhadan <21gehassig@gmail.com>
2023-12-14 15:29:15 +0200

    refactor book and user

--- borrowing/models.py
+++ borrowing/models.py
@@ -8,8 +8,8 @@ class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
 
     def clean(self):
         if self.borrow_date > self.expected_return_date:
