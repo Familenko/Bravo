378483e6aa167ac817f3a4afd3ea8d46fd96019c
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-12-14 16:26:48 +0100

    Attach the current user to the borrowing, Implement and create an endpoint

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets, mixins
 
 from borrowing.models import Borrowing
@@ -10,5 +8,21 @@ from borrowing.serializers import (
 )
 
 
-class BorrowingViewSet():
-    pass
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
