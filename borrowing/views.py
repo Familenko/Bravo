4791adb8d9d639a229afbe7ae8305d2674e1363e
Roman Zhadan <21gehassig@gmail.com>
2023-12-14 16:07:03 +0200

    change views from ViewSet to generics style

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets, mixins
+from rest_framework import generics
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -10,23 +8,11 @@ from borrowing.serializers import (
 )
 
 
-class BorrowingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-    queryset = Borrowing.objects.all()
+class BorrowingListView(generics.ListCreateAPIView):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingListSerializer
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return BorrowingListSerializer
 
-        elif self.action == "retrieve":
-            return BorrowingDetailSerializer
-
-        return BorrowingSerializer
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-    def perform_update(self, serializer):
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        instance.delete()
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingDetailSerializer
