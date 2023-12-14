f792f1a27d3b47118a5a2b34c274f6a747908b32
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-14 09:06:23 +0200

    init models

--- library_config/settings.py
+++ library_config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # custom apps
+    "book",
+    "user",
+    "borrowing",
+    "payment",
 ]
 
 MIDDLEWARE = [
--- library_config/urls.py
+++ library_config/urls.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path(
+        "api/book/",
+        include("book.urls",
+                namespace="book")),
+
+    path(
+        "api/borrowing/",
+        include("borrowing.urls",
+                namespace="borrowing")),
+
+    path(
+        "api/payment/",
+        include("payment.urls",
+                namespace="payment")),
+
+    path(
+        "api/user/",
+        include("user.urls",
+                namespace="user")),
 ]
