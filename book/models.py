c8e8345a538edc6c564d7a20dfa647c886fcde78
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-12-14 14:36:22 +0200

    Add book model to library service

--- book/models.py
+++ book/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 class Book(models.Model):
-    pass
+    COVER_TYPES = (
+        ("HARD", "Hard"),
+        ("SOFT", "Soft"),
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(choices=COVER_TYPES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_length=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
