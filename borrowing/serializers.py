8e0de2794c043946533a015bcdbcd6899f94b9b3
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-12-14 15:41:25 +0100

    Create serializer, Validate book inventory, Decrease inventory

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,10 +1,43 @@
 from rest_framework import serializers
 
+import book
+
 from borrowing.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        ]
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        user = self.context["request"].user
+
+        if not user.is_authenticated:
+            raise serializers.ValidationError("User must be authenticated to borrow a book.")
+
+        if book.inventory == 0:
+            raise serializers.ValidationError("Book is out of stock.")
+
+        borrowing = Borrowing.objects.create(
+            expected_return_date=validated_data["expected_return_date"],
+            actual_return_date=validated_data["actual_return_date"],
+            book=book,
+            user=user
+        )
+
+        book.inventory -= 1
+        book.save()
+
+        return borrowing
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
