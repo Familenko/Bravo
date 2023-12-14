1619eba668c745ef73529027812a57eed7b8fbdd
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-12-14 16:55:29 +0200

    Create views for book model

--- book/views.py
+++ book/views.py
@@ -3,16 +3,21 @@ from rest_framework import generics
 from book.models import Book
 from book.serializers import (
     BookSerializer,
-    BookListSerializer,
-    BookDetailSerializer,
 )
 
 
-class BookListView(generics.ListAPIView):
+class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookListSerializer
+    serializer_class = BookSerializer
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookDetailSerializer
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        title = self.request.query_params.get("title", None)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
