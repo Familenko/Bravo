133a29e4586201314ddda988d8922e91c26d6b1c
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-18 11:39:09 +0200

    fix stripe_test_func

--- payment/stripe.py
+++ payment/stripe.py
@@ -2,12 +2,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 import stripe
 
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-stripe_api_key = os.getenv("STRIPE_API_KEY")
+stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
 @csrf_exempt
@@ -17,15 +16,15 @@ def create_checkout_test_session(request):
             'price_data': {
                 'currency': 'usd',
                 'product_data': {
-                    'name': 'Book3',
+                    'name': 'Book4',
                 },
                 'unit_amount': 3000,
             },
             'quantity': 1,
         }],
         mode='payment',
-        success_url='http://127.0.0.0:8000/',
-        cancel_url='http://127.0.0.0:8000/',
+        success_url='http://localhost:8000/success',
+        cancel_url='http://localhost:8000/cancel',
     )
 
     return HttpResponseRedirect(session.url, status=303)
