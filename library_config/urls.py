6ad1aaaa694f2bc25e5ae43a5406843e129d5e53
Kateryna Koziar <katya.kozyar.333@gmail.com>
2024-01-06 17:27:21 +0200

    Fix formatting for urls

--- library_config/urls.py
+++ library_config/urls.py
@@ -8,26 +8,22 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path(
         "api/book/",
-        include("book.urls",
-                namespace="book")),
-
+        include("book.urls", namespace="book")
+    ),
     path(
         "api/borrowing/",
-        include("borrowing.urls",
-                namespace="borrowing")),
-
+        include("borrowing.urls", namespace="borrowing")
+    ),
     path(
         "api/payment/",
-        include("payment.urls",
-                namespace="payment")),
-
+        include("payment.urls", namespace="payment")
+    ),
     path(
         "api/user/",
-        include("user.urls",
-                namespace="user")),
+        include("user.urls", namespace="user")
+    ),
 ]
 
 # documentation
@@ -39,7 +35,8 @@ urlpatterns += [
         name="swagger-ui",
     ),
     path(
-        "api/doc/redoc/", SpectacularRedocView.as_view(
-            url_name="schema"), name="redoc"
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
     ),
 ]
